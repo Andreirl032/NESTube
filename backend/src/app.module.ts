734f9cb88a8469b0460f3b862bca76aa772aa2b4
Andreirl032 <andrei.ramos.lopes@gmail.com>
2026-09-14 14:45:14 -0300

    created module resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+import { WatchHistoryModule } from './watch_history/watch_history.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, VideoModule, WatchHistoryModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
